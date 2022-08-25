30c5c5e8979590748fb3da3fee55ae8306694e8c
YazeedGhazalat <yazeedghazalat@gmail.com>
2022-08-25 17:45:56 +0300

    My first update

--- main.dart
+++ main.dart
@@ -1,12 +1,11 @@
 void main() {
-    Map MooInfo = {
+  Map MooInfo = {
     "Fname": "Yazeed",
     "Lname": "Ghazalat",
     "age": 23,
     "nationallity": 'jordanian',
     "favTaeam": "Real madrid",
     "best color": 'red',
-    
     "eduInfo": {"name": "JUST", "faculty": "IT", "year": "2022"}
   };
   print(MooInfo);

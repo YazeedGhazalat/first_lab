0c2f628190a31ee4615180cec80b3db3e9c9f3a4
YazeedGhazalat <yazeedghazalat@gmail.com>
2022-08-25 17:44:13 +0300

    My first update

---
+++ main.dart
@@ -0,0 +1,13 @@
+void main() {
+    Map MooInfo = {
+    "Fname": "Yazeed",
+    "Lname": "Ghazalat",
+    "age": 23,
+    "nationallity": 'jordanian',
+    "favTaeam": "Real madrid",
+    "best color": 'red',
+    
+    "eduInfo": {"name": "JUST", "faculty": "IT", "year": "2022"}
+  };
+  print(MooInfo);
+}
